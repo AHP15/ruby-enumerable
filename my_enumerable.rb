e81464f5cd528d5e7a1cc005bcb76154c0466bf5
abdessittir harkaty <abdessattarharkaty@gmail.com>
2023-01-26 11:09:40 +0100

    Add the MyList class and MyEnumerable module

---
+++ my_enumerable.rb
@@ -0,0 +1,37 @@
+module MyEnumerable
+
+  def defaultCondition(element)
+    element
+  end
+
+  def all(&condition)
+    each do |element|
+      if !condition.call(element)
+        return false
+      end
+    end
+
+    true
+  end
+
+  def any(&condition)
+    each do |element|
+      if condition.call(element)
+        return true
+      end
+    end
+
+    false
+  end
+
+  def filter(&condition)
+    result = []
+    each do |element|
+      if condition.call(element)
+        result.push(element)
+      end
+    end
+
+    result
+  end
+end
